171a60b735741674d7cb7a72751f29be5b0a0eba
mouadispunk <bencaidmouad9@gmail.com>
2024-05-08 02:23:30 +0100

    update

--- install-config.sh
+++ install-config.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
+echo "nginx....." 
+apt-get update
+apt-get install -y nginx
+
+echo "php....." 
+apt-get install -y \
+		php8.3-fpm \
+		php8.3-mysql \
+		php8.3-curl \
+		php8.3-xml \
+		php8.3-mbstring \
+		php8.3-zip \
+		php8.3-intl \
+		php8.3-cli 
+
+echo "mysql-server....." 
+apt-get install -y mysql-server
+
+echo "phpmyadmin....."
+apt-get install -y phpmyadmin
 
 echo "Enabling services..."
 service nginx start
 service mysql start
 service php8.3-fpm start
+
+echo "------------------------------------Installation complete. Services have been enabled."
 
 echo "Configuring Nginx to serve PHP applications..."
 
@@ -23,9 +45,10 @@ FLUSH PRIVILEGES;
 CREATE DATABASE db;
 exit
 "
+
 echo "$SQL_COMMANDS" | mysql
 
 sudo sed -i "s/localhost/127.0.0.1/g" /etc/phpmyadmin/config-db.php
 nano /etc/phpmyadmin/config-db.php
 
-echo "Installation and configuration complete. Services have been enabled."
+echo "---------------------------------configuration complete"
