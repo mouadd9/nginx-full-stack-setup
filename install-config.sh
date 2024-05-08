9a94f2e715bbdb75f75d781cd3eb2cb30d013cf3
mouadispunk <bencaidmouad9@gmail.com>
2024-05-08 03:06:01 +0100

    update

--- install-config.sh
+++ install-config.sh
@@ -1,46 +1,28 @@
 #!/bin/bash
-echo "nginx....." 
+echo "Updating system..."
 apt-get update
-apt-get install -y nginx
+apt-get install -y nginx php8.3-fpm php8.3-mysql php8.3-curl php8.3-xml php8.3-mbstring php8.3-zip php8.3-intl php8.3-cli mysql-server
 
-echo "php....." 
-apt-get install -y \
-		php8.3-fpm \
-		php8.3-mysql \
-		php8.3-curl \
-		php8.3-xml \
-		php8.3-mbstring \
-		php8.3-zip \
-		php8.3-intl \
-		php8.3-cli 
-
-echo "mysql-server....." 
-apt-get install -y mysql-server
-
-echo "Preseeding debconf to skip phpMyAdmin dbconfig-common..."
-echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect' | debconf-set-selections
+echo "Preseeding debconf for phpMyAdmin..."
+echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect none' | debconf-set-selections
 echo 'phpmyadmin phpmyadmin/dbconfig-install boolean false' | debconf-set-selections
 
-echo "phpmyadmin....."
+echo "Installing phpMyAdmin..."
 apt-get install -y phpmyadmin
 
-echo "Enabling services..."
+echo "Services are being started..."
 service nginx start
 service mysql start
 service php8.3-fpm start
 
-echo "------------------------------------Installation complete. Services have been enabled."
-
-echo "Configuring Nginx to serve PHP applications..."
-
+echo "Configuring Nginx for PHP applications..."
 cp /app/nginx.conf /etc/nginx/sites-available/default
 rm -f /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 nginx -t
 service nginx restart
 
-echo "Updating MySQL Privileges..."
-
+echo "Configuring MySQL..."
 SQL_COMMANDS="
 CREATE USER IF NOT EXISTS 'phpmyadmin'@'localhost' IDENTIFIED BY 'password';
 ALTER USER 'phpmyadmin'@'localhost' IDENTIFIED BY 'password';
@@ -49,10 +31,9 @@ FLUSH PRIVILEGES;
 CREATE DATABASE db;
 exit
 "
-
-echo "$SQL_COMMANDS" | mysql
+echo "$SQL_COMMANDS" | mysql -u root -p
 
 sudo sed -i "s/localhost/127.0.0.1/g" /etc/phpmyadmin/config-db.php
 nano /etc/phpmyadmin/config-db.php
 
-echo "---------------------------------configuration complete"
+echo "Configuration complete!"
