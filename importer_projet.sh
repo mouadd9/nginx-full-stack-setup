22278069d9048b988e4250a7d6f7d333dbe02eec
mouadispunk <bencaidmouad9@gmail.com>
2024-05-09 08:01:10 +0100

    test

--- importer_projet.sh
+++ importer_projet.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+
+# Check if an argument is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <url>"
+    exit 1
+fi
+
+# Take the URL from the first argument
+URL=$1
+
+# Remove the existing directory
 rm -r /var/www/html*
-git clone https://github.com/KIHLYouns/OS_Form.git /var/www/html
+
+# Clone the repository from the provided URL into /var/www/html
+git clone $URL /var/www/html
+
+
