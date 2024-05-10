f5c1d1ebc47916aa9ff4894e40cdc46d6ad61bac
mouadispunk <bencaidmouad9@gmail.com>
2024-05-10 07:57:39 +0100

    update

--- install-config.sh
+++ install-config.sh
@@ -30,9 +30,9 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get install -y phpmyadmin
 
 echo "Enabling services..."
-service nginx start
-service mysql start
-service php8.3-fpm start
+service nginx restart
+service mysql restart
+service php8.3-fpm restart
 
 # Enabling PHP extensions
 phpenmod mbstring
@@ -63,30 +63,22 @@ DEBIAN_MAINT_USER=$(grep 'user' /etc/mysql/debian.cnf | head -1 | awk -F' = ' '{
 # Configure debian-sys-maint user in MySQL
 mysql -u root -e "
 CREATE USER IF NOT EXISTS '${DEBIAN_MAINT_USER}'@'localhost' IDENTIFIED BY '${DEBIAN_MAINT_PASSWORD}';
-GRANT ALL PRIVILEGES ON *.* TO '${DEBIAN_MAINT_USER}'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON . TO '${DEBIAN_MAINT_USER}'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 "
 
-
-
-echo "Updating MySQL Privileges and Creating New User..."
-
-# Prompt user for MySQL new user credentials
-echo "Enter new MySQL user name:"
-read NEW_USER
-echo "Enter password for new MySQL user:"
-read NEW_PASS
+mysql -u root "
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('passroot');
+FLUSH PRIVILEGES;
+"
 
 # Configure new MySQL user and grant privileges
-mysql -u root -e "
-CREATE USER IF NOT EXISTS '${NEW_USER}'@'127.0.0.1' IDENTIFIED BY '${NEW_PASS}';
-GRANT ALL PRIVILEGES ON *.* TO '${NEW_USER}'@'127.0.0.1' WITH GRANT OPTION;
+mysql -u root -p passroot -e "
+CREATE USER IF NOT EXISTS 'user'@'localhost' IDENTIFIED BY 'pass';
+GRANT ALL PRIVILEGES ON . TO 'user'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS db;
 "
-
-# Ensure phpMyAdmin uses IP address instead of 'localhost'
-sudo sed -i "s/'localhost'/'127.0.0.1'/g" /etc/phpmyadmin/config-db.php
 
 echo "Now manually configure phpMyAdmin settings by editing config-db.php"
 
