3b5d9b4dd96b230fea6b291da7b14b88c917e500
mouadispunk <bencaidmouad9@gmail.com>
2024-05-10 18:16:21 +0100

    added some changes in the script that imports the project

--- importer_projet.sh
+++ importer_projet.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-# Check if an argument is provided
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <url>"
     exit 1
 fi
 
-# Take the URL from the first argument
 URL=$1
 
-# Remove the existing directory
-rm -r /var/www/html*
+echo "Entrer le nom du fichier du projet :"
+read PROJECT_FOLDER
 
-# Clone the repository from the provided URL into /var/www/html
-git clone $URL /var/www/html/proj
+FULL_PATH="/var/www/html/$PROJECT_FOLDER"
+
+rm -rf "$FULL_PATH"
+
+git clone "$URL" "$FULL_PATH"
+
+echo "Projet importé ici $FULL_PATH"
 
 
